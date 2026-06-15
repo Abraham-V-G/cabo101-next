1c6fb075e67af018b4618d374c684ff32cf233d9
Edgar Abraham Gutierrez Venegas <AbrahamVenegas@MacBook-Air-2.local>
2026-06-14 17:53:33 -0700

    Add admin middleware auth.4

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -6,21 +6,35 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  console.log("EMAIL RECIBIDO:", email);
-  console.log("EMAIL ENV:", process.env.ADMIN_EMAIL);
+  console.log("========== LOGIN ==========");
+  console.log("EMAIL RECIBIDO:", JSON.stringify(email));
+  console.log("EMAIL ENV:", JSON.stringify(process.env.ADMIN_EMAIL));
 
-  console.log("PASSWORD RECIBIDO:", password);
-  console.log("PASSWORD ENV:", process.env.ADMIN_PASSWORD);
+  console.log("PASSWORD RECIBIDO:", JSON.stringify(password));
+  console.log("PASSWORD ENV:", JSON.stringify(process.env.ADMIN_PASSWORD));
+
+  console.log(
+    "EMAIL MATCH:",
+    email === process.env.ADMIN_EMAIL
+  );
+
+  console.log(
+    "PASSWORD MATCH:",
+    password === process.env.ADMIN_PASSWORD
+  );
 
   if (
     email !== process.env.ADMIN_EMAIL ||
     password !== process.env.ADMIN_PASSWORD
   ) {
+    console.log("LOGIN FAILED");
     return NextResponse.json(
       { success: false },
       { status: 401 }
     );
   }
+
+  console.log("LOGIN SUCCESS");
 
   const token = jwt.sign(
     {
